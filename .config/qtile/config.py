646a918ba0114efb696ea33e10c41fda5441b1ec
Pollot <79442640+Pollot@users.noreply.github.com>
2022-10-17 15:54:06 +0200

    Update config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -183,27 +183,27 @@ groups = [
 for i in groups:
     keys.extend(
         [
-            # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
-            # mod1 + control + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "control"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # mod1 + shift + letter of group = move focused window to group
-            Key(
-            	[mod, "shift"], 
-            	i.name, 
-            	lazy.window.togroup(i.name),
-                desc="move focused window to group {}".format(i.name),
-            ),
+        # mod1 + letter of group = switch to group
+        Key(
+            [mod],
+            i.name,
+            lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name),
+        ),
+        # mod1 + control + letter of group = switch to & move focused window to group
+        Key(
+            [mod, "control"],
+            i.name,
+            lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name),
+        ),
+		# mod1 + shift + letter of group = move focused window to group
+        Key(
+            [mod, "shift"], 
+            i.name, 
+            lazy.window.togroup(i.name),
+            desc="move focused window to group {}".format(i.name),
+        ),
         ]
     )
 
