be28dca35e8e21a01996a4d149e2d31329391cbf
Pollot <79442640+Pollot@users.noreply.github.com>
2022-10-29 19:37:37 +0200

    OpenWeatherMap symbols remap to Nerd Font added

---
+++ .config/qtile/owm.py
@@ -0,0 +1,25 @@
+# Your OpenWeatherMap API goes here
+owm_api = "7834197c2338888258f8cb94ae14ef49"
+
+# OpenWeatherMap symbols remap for Nerd Font compatibility
+owm_symbols = {
+    "Unknown": "",  # nf-fa-question
+    "01d": "",      # nf-weather-day_sunny
+    "01n": "",      # nf-weather-moon_full
+    "02d": "",      # nf-weather-day_cloudy
+    "02n": "",      # nf-weather-night_cloudy
+    "03d": "",      # nf-weather-cloud
+    "03n": "",      # nf-weather-cloud
+    "04d": "",      # nf-weather-cloudy
+    "04n": "",      # nf-weather-cloudy
+    "09d": "",      # nf-weather-day_showers
+    "09n": "",      # nf-weather-night_showers
+    "10d": "",      # nf-weather-rain
+    "10n": "",      # nf-weather-rain
+    "11d": "",      # nf-weather-thunderstorm
+    "11n": "",      # nf-weather-thunderstorm
+    "13d": "",      # nf-weather-snow
+    "13n": "",      # nf-weather-snow
+    "50d": "",      # nf-weather-fog
+    "50n": "",      # nf-weather-fog
+}
--- .config/qtile/owm_api.py
+++
@@ -1,2 +0,0 @@
-# Your OpenWeatherMap API goes here
-owm_api = "7834197c2338888258f8cb94ae14ef49"
