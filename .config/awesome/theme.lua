ab57015e6c90d5a25dfbd6cbd970eb41df68c6e7
Pollot737 <olaf.moroz.mirtex@gmail.com>
2022-11-28 11:38:47 +0100

    Wallpaper added

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -1,6 +1,6 @@
-------
---  --
-------
+-----------------
+--- Libraries ---
+-----------------
 
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
@@ -135,7 +135,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path.."default/background.png"
+theme.wallpaper = "~/Wallpapers/city.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
