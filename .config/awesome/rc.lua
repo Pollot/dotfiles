bccdf06e1ce814632a07f533e6ec356ca613ce2d
Pollot737 <olaf.moroz.mirtex@gmail.com>
2022-12-03 12:37:20 +0100

    Different border for floating windows added

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -569,9 +569,23 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
+client.connect_signal("focus", function(c) 
+    if c.floating then
+        c.border_color = beautiful.border_floating
+    else
+        c.border_color = beautiful.border_focus
+    end
+end)
+
+client.connect_signal("property::floating", function(c) 
+    if c.floating then
+        c.border_color = beautiful.border_floating
+    else
+        c.border_color = beautiful.border_focus
+    end
+end)
 
 -------------------
 ---- Autostart ----
